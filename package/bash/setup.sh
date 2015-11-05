59bc1e1e1fdee6c7ad982da87173cb1730c85185
zhujs <zhujs.w@outlook.com>
2015-11-05 16:04:45 +0800

    Rename the configuration files

--- package/bash/setup.sh
+++ package/bash/setup.sh
@@ -1,8 +1,8 @@
 BASH_DIR="$( (cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd) )"
 
-createSymlink "$BASH_DIR/.bashrc" ~/.bashrc
-createSymlink "$BASH_DIR/.bash_config" ~/.bash_config
+createSymlink "$BASH_DIR/bashrc" ~/.bashrc
+createSymlink "$BASH_DIR/bash_config" ~/.bash_config
 
-createSymlink "$BASH_DIR/.inputrc" ~/.inputrc
+createSymlink "$BASH_DIR/inputrc" ~/.inputrc
 
 unset BASH_DIR
--- package/vim/setup.sh
+++ package/vim/setup.sh
@@ -6,6 +6,6 @@ type vim > /dev/null 2>&1 || installpackage "vim"
 type git &> /dev/null || installpackage "git"
 # get the Vundle plungin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-createSymlink "$VIM_DIR/.vimrc" ~/.vimrc
+createSymlink "$VIM_DIR/vimrc" ~/.vimrc
 
 unset VIM_DIR
