20a491f8fe0ebbe84ef535ee28a116326d4c1c43
zhujs <zhujs.w@outlook.com>
2015-11-09 16:40:53 +0800

    Add a customized configuration file for vim
    Also add a key mappaing for substitution command `s` in vim

--- package/vim/setup.sh
+++ package/vim/setup.sh
@@ -7,5 +7,6 @@ type git &> /dev/null || installpackage "git"
 # get the Vundle plungin
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 createSymlink "$VIM_DIR/vimrc" ~/.vimrc
+createSymlink "$VIM_DIR/vim_config" ~/.vim_config
 
 unset VIM_DIR
