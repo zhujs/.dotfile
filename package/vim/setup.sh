688bd05bc4ab3a5014abaf0596f8c537f63a481f
zhujs <zhujs.w@outlook.com>
2016-01-12 20:40:40 +0800

    Create directory for vim-vundle

--- package/vim/setup.sh
+++ package/vim/setup.sh
@@ -4,7 +4,9 @@ VIM_DIR="$( (cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd) )"
 type vim > /dev/null 2>&1 || installpackage "vim"
 
 type git &> /dev/null || installpackage "git"
+
 # get the Vundle plungin
+mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 createSymlink "$VIM_DIR/vimrc" ~/.vimrc
 
