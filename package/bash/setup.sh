6a2d703319b61dff3cc76b985e7c262f29d309ae
zhujs <zhujs.w@outlook.com>
2016-01-13 19:09:49 +0800

    Update the installed bash in OSX

--- package/bash/setup.sh
+++ package/bash/setup.sh
@@ -1,5 +1,11 @@
 BASH_DIR="$( (cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd) )"
 
+# update bash version in OSX
+if [ "$(uname)x" == "Darwin" ]
+then
+    brew install bash
+fi
+
 createSymlink "$BASH_DIR/bashrc" ~/.bashrc
 createSymlink "$BASH_DIR/bash_config" ~/.bash_config
 createSymlink "$BASH_DIR/profile" ~/.profile
