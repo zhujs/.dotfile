803d792d8311c8f9fd40416dabcca0e8c7fb7eac
zhujs <zhujs.w@outlook.com>
2016-01-12 22:26:23 -0900

    Setup the login profile for bash
    Unify the start-up behavior of bash in Linux and Mac

--- package/bash/setup.sh
+++ package/bash/setup.sh
@@ -2,6 +2,7 @@ BASH_DIR="$( (cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd) )"
 
 createSymlink "$BASH_DIR/bashrc" ~/.bashrc
 createSymlink "$BASH_DIR/bash_config" ~/.bash_config
+createSymlink "$BASH_DIR/profile" ~/.profile
 
 createSymlink "$BASH_DIR/inputrc" ~/.inputrc
 
