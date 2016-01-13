01a8464452e57415f14e921cf3936c6c243a7e4a
zhujs <zhujs.w@outlook.com>
2016-01-13 13:46:56 +0800

    Setup packages
    Install bash-completion and reattach-to-user-namespace

---
+++ osx/installPackage.sh
@@ -0,0 +1,6 @@
+# tool to access the OSX's clipboard in tmux
+#     http://www.economyofeffort.com/2013/07/29/reattach-to-user-namespace-the-fix-for-your-tmux-in-os-x-woes/
+brew install reattach-to-user-namespace
+
+# bash completion
+brew install bash-completion
