c170fb2f3da6990b5164bd3cf73ca668cafbe2a1
zhujs <zhujs.w@outlook.com>
2016-01-13 14:17:20 +0800

    Remove unnecessary warning messages

--- osx/installPackage.sh
+++ osx/installPackage.sh
@@ -1,6 +1,6 @@
 # tool to access the OSX's clipboard in tmux
 #     http://www.economyofeffort.com/2013/07/29/reattach-to-user-namespace-the-fix-for-your-tmux-in-os-x-woes/
-brew install reattach-to-user-namespace
+type reattach-to-user-namespace &> /dev/null || brew install reattach-to-user-namespace
 
 # bash completion
-brew install bash-completion
+type bash-completion || brew install bash-completion
