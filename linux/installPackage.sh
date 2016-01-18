7dc42dae0c50fc6e84f93f57ce6e025d8103b3a1
zhujs <zhujs.w@outlook.com>
2016-01-18 12:14:59 +0800

    Make sudo to prompt password only once (hopefully)

--- linux/installPackage.sh
+++ linux/installPackage.sh
@@ -1,3 +1,12 @@
+
+# Ask for the administrator password upfront.
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
+# Nice trick to keep the password alive, but it fails due to race condition
+#   https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # install xclip
 type xclip || installpackage xlcip
 
