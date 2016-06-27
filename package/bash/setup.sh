49f250e3c9d1783dc6672a9f94108d6902f132f3
zhujs <zhujs.w@outlook.com>
2016-06-27 15:31:10 +0800

    Split bash_prompt from bashrc

--- package/bash/setup.sh
+++ package/bash/setup.sh
@@ -8,6 +8,7 @@ fi
 
 createSymlink "$BASH_DIR/bashrc" ~/.bashrc
 createSymlink "$BASH_DIR/bash_config" ~/.bash_config
+createSymlink "$BASH_DIR/bash_prompt" ~/.bash_prompt
 createSymlink "$BASH_DIR/profile" ~/.profile
 
 createSymlink "$BASH_DIR/inputrc" ~/.inputrc
