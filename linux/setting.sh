82c367659563c4a4623bf7401de5c34b0c15cf3a
zhujs <zhujs.w@outlook.com>
2016-09-05 06:48:12 -0400

    Add command to check all gsettings keys

--- linux/setting.sh
+++ linux/setting.sh
@@ -1,4 +1,5 @@
 # setup shortcut keybindings
+# gsettings list-schemas |  gsettings list-recursively | less
 gsettings set org.gnome.desktop.wm.keybindings close "['<Alt>w']"
 
 gsettings set org.gnome.settings-daemon.plugins.media-keys terminal "<Alt>j"
