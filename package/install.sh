23c2a5ba53a70cd78ed06a1fe8da203b01e6428a
zhujs <zhujs.w@outlook.com>
2016-09-29 05:53:52 -0400

    Remove codes

--- package/install.sh
+++ package/install.sh
@@ -5,7 +5,6 @@ SCRIPT_DIR="$( (cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd) )"
 for name in $(\ls $SCRIPT_DIR)
 do
     [ -d "$SCRIPT_DIR/$name" ] && [ -f "$SCRIPT_DIR/$name/setup.sh" ] \
-    echo "Setting up ${name}..."
     && source "$SCRIPT_DIR/$name/setup.sh"
 done
 
